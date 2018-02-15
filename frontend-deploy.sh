df997b652338c23b4c07bd6e82f2a2e92cff0208
Dawid Grochowski <dgrochowski@pgs-soft.com>
2018-02-15 18:52:30 +0100

    Update Travis config (#20)

--- frontend-deploy.sh
+++ frontend-deploy.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 if [[ $TRAVIS_BRANCH == 'master' ]]
+then
   git clone git@github.com:dgrochowski/dgrochowski.github.io.git deploy
   cd deploy
-  git checkout gh_pages
   rm -rf frontend
   mkdir frontend
   cp -r ../css ./frontend/css
   cp -r ../html5 ./frontend/html5
-  cp -r ../dist ./frontend/dist
-  cp -r ../node_modules ./frontend/node_modules
-  cp -r ../sass ./frontend/sass
-  cp -r ../webpack ./frontend/webpack
-  cp -r ../templates ./frontend/templates
+#  cp -r ../dist ./frontend/dist
+#  cp -r ../node_modules ./frontend/node_modules
+#  cp -r ../sass ./frontend/sass
+#  cp -r ../webpack ./frontend/webpack
+#  cp -r ../templates ./frontend/templates
   git add *
   git reset HEAD~1 --soft
   git commit -m "Frontend upskill pages"
-  git push origin gh_pages -f
+  git push origin master -f
 fi
